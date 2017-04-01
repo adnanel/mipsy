1781d54005a7393f298cc5ea05772f8f8a69425a
adnanel <adnanel94@gmail.com>
2017-04-01 23:43:11 +0200

    Implementiran ALU Controller

--- src/mipsy/core/components/ALUControllerComponent.java
+++ src/mipsy/core/components/ALUControllerComponent.java
@@ -1,10 +1,16 @@
 package mipsy.core.components;
 
+import mipsy.Utility;
+
+import java.util.function.Consumer;
+
 /**
  * Created by Adnan on 4/1/2017.
  */
 public class ALUControllerComponent {
-    private int aluOp;
+    // http://prntscr.com/erb20h
+
+    private int aluOp; //2 bit
     private int instruction; // instruction[5:0]
 
     private String name;
@@ -17,7 +23,24 @@ public class ALUControllerComponent {
         this.aluOp = aluOp;
     }
 
-    public int getResult() {
+    public int getResult(Consumer<String> logger) {
+        aluOp = Utility.SubBits(aluOp, 0, 2);
+
+        if ( aluOp == 0 )       return 0b0010;
+        if ( aluOp % 2 == 1 )   return 0b0110;
+
+        if ( (aluOp >> 1) > 0 ) {
+            instruction <<= 2;
+            instruction >>= 2;
+
+            if ( instruction == 0 ) return 0b0010;
+            if ( instruction == 2 ) return 0b0110;
+            if ( instruction == 4 ) return 0b0000;
+            if ( instruction == 5 ) return 0b0001;
+            if ( instruction == 10) return 0b0111;
+        } else {
+            logger.accept(String.format("%s: Undefined ALUOp combination!", name));
+        }
         return 0; //todo
     }
 
