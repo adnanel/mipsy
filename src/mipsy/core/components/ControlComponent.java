34410803e6da710e7d1c753155aa5d2ebdd40740
haris_h <hhalilovic1@etf.unsa.ba>
2017-04-01 14:13:58 +0200

    asd

--- src/mipsy/core/components/ControlComponent.java
+++ src/mipsy/core/components/ControlComponent.java
@@ -4,4 +4,20 @@ package mipsy.core.components;
  * Created by Adnan on 3/31/2017.
  */
 public class ControlComponent {
+
+    //http://prnt.sc/er3zsn
+
+    public static final int CONTROL_AND = 0;  // 0000
+
+    public int CC_OUT0;
+    public int CC_OUT1;
+    public int CC_OUT2;
+
+    public int selector;
+
+    public void setSelector(int selector) {
+        this.selector = selector;
+    }
+
+
 }
