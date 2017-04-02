3f3564171e0a4b8545c1c6694fd482b9a9edc0d7
adnanel <adnanel94@gmail.com>
2017-04-02 21:16:46 +0200

    Implementiran LW

--- src/mipsy/instructions/InstructionLw.java
+++ src/mipsy/instructions/InstructionLw.java
@@ -1,6 +1,8 @@
 package mipsy.instructions;
 
+import mipsy.Utility;
 import mipsy.types.Instruction;
+import mipsy.types.Register;
 
 import java.util.List;
 
@@ -10,12 +12,49 @@ import java.util.List;
 
 //todo
 public class InstructionLw extends Instruction {
+    // example: lw  $s2, 0($a2)
+    //          lw  rt, offset(base)
+
+    private String destReg;
+    private int offset;
+    private String baseReg;
+
     protected InstructionLw(List<String> args) {
         super(args);
+
+        this.instruction = "lw";
+
+        if ( args.size() != 2 )
+            throw new IllegalArgumentException("Invalid arguments passed to lw! Expected 2, given " + args.size());
+
+        destReg = args.get(0);
+
+        //drugi arg se sastoji od offset(base)
+
+        offset = Utility.ParseInt(args.get(1).substring(0, args.get(1).indexOf('(')));
+
+        String fin = args.get(1);
+        fin = fin.substring(fin.indexOf('(') + 1);
+        fin = fin.substring(0, fin.length() - 1);
+
+        baseReg = fin;
     }
 
     @Override
     public int getCoded() {
-        return 0;
+        int res = 0b100011;
+
+        res = (res << 6) | Register.getRegisterNumber(baseReg);
+
+        res = (res << 5) | Register.getRegisterNumber(destReg);
+
+        res = (res << 5) | offset;
+
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("lw %s, %d(%s)", destReg, offset, baseReg);
     }
 }
